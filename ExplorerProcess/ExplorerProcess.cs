1709f1c1add3a37ac6e02475a03be1a7d55783e3
chosle <chosle90@gmail.com>
2024-08-25 19:25:11 +0900

    ExplorerManager: SetWindowPos 함수 및 관련 메서드 추가
    `user32.dll`의 `SetWindowPos` 함수를 가져오도록 변경되었습니다.
    필요한 상수와 핸들이 추가되었습니다.
    `SetTopMost` 메서드가 추가되어 특정 창을 항상 위에 있도록 설정할 수 있습니다.
    `SetWindowTopForMoment` 메서드가 추가되어 특정 창을 잠시 동안 위에 있도록 설정한 후 다시 원래 상태로 되돌릴 수 있습니다.

--- ExplorerProcess/ExplorerProcess.cs
+++ ExplorerProcess/ExplorerProcess.cs
@@ -12,7 +12,6 @@ namespace ExplorerManager
         private readonly int maxWaitTimeInMilliseconds = 2000;
         private readonly int pollingIntervalInMilliseconds = 50;
 
-
         #region user32 dll imports
         private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
 
@@ -27,6 +26,16 @@ namespace ExplorerManager
 
         [DllImport("user32.dll")]
         private static extern bool IsWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
+        private static readonly IntPtr HWND_TOPMOST = new IntPtr(-1);
+        private static readonly IntPtr HWND_NOTOPMOST = new IntPtr(-2);
+        private const uint SWP_NOSIZE = 0x0001;
+        private const uint SWP_NOMOVE = 0x0002;
+        private const uint SWP_NOACTIVATE = 0x0010;
+        private const uint SWP_SHOWWINDOW = 0x0040;
         #endregion
 
         public delegate void ExplorerClosedCallback();
@@ -86,6 +95,46 @@ namespace ExplorerManager
             {
                 return null;
             }
+        }
+
+        public bool SetTopMost()
+        {
+            if (explorerWindowHandle == IntPtr.Zero ||
+                !IsWindow(explorerWindowHandle) ||
+                !IsWindowVisible(explorerWindowHandle))
+            {
+                return false;
+            }
+
+            return SetWindowPos(
+                explorerWindowHandle,
+                HWND_TOPMOST,
+                0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
+        }
+
+        public bool SetWindowTopForMoment()
+        {
+            if (explorerWindowHandle == IntPtr.Zero ||
+                !IsWindow(explorerWindowHandle) ||
+                !IsWindowVisible(explorerWindowHandle))
+            {
+                return false;
+            }
+
+            SetWindowPos(
+                explorerWindowHandle,
+                HWND_TOPMOST,
+                0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
+
+            Thread.Sleep(100);
+
+            return SetWindowPos(
+                explorerWindowHandle,
+                HWND_NOTOPMOST,
+                0, 0, 0, 0,
+                SWP_NOMOVE | SWP_NOSIZE | SWP_NOACTIVATE | SWP_SHOWWINDOW);
         }
 
         private void CheckIfWindowClosed(object state)
