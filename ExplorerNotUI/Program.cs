bf3f258893d2e6cf349549c0d42517bf0f1175f6
chosle <chosle90@gmail.com>
2024-08-25 21:02:12 +0900

    콘솔 출력 제거: 탐색기 창 상태 메시지
    탐색기 창이 닫히거나 새로 열렸을 때 콘솔에 출력되는
    메시지들이 제거되었습니다. 이는 로그의 간결성을
    유지하고 불필요한 출력물을 줄이기 위함입니다.

--- ExplorerNotUI/Program.cs
+++ ExplorerNotUI/Program.cs
@@ -9,19 +9,13 @@ namespace ExplorerManager
             var explorerProcessManager = new ExplorerProcessManager();
             explorerProcessManager.OnExplorerClosed += (hWnd) =>
             {
-                Console.WriteLine("Explorer window has closed. Exiting application.");
                 Application.Exit();
             };
 
             var explorer = explorerProcessManager.StartExplorer();
             if (explorer != IntPtr.Zero)
             {
-                Console.WriteLine($"New explorer.exe window found with handle: {explorer}");
                 Application.Run();
-            }
-            else
-            {
-                Console.WriteLine("No new explorer.exe window was found.");
             }
         }
     }
